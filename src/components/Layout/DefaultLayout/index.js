35a6495d01a3ba05dffc3d618e8168667eef0d11
Trần Ngọc Hoàn <thedevil0903@gmail.com>
2023-02-15 16:19:09 +0700

    Cấu hình Router/Layout

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,15 @@
+import Header from '../../Layout/components/Header';
+import Sidebar from './Sidebar';
+
+function DefaultLayout({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <Sidebar />
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
+export default DefaultLayout;
