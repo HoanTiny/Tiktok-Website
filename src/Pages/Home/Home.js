5b7f1045d21a0d52df9e9a7c68a2bddad1b664ec
Hoan Tiny <thedevil093@gmail.com>
2023-12-04 17:27:49 +0700

    update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
+// Home.js
 import classNames from 'classnames/bind';
 import styles from './Home.module.scss';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import * as VideoListForYou from 'src/services/videoListForYouService';
 
 import Video from '~/components/Video/Video';
@@ -9,6 +10,9 @@ const cx = classNames.bind(styles);
 
 function Home() {
     const [videoForYou, setVideoForYou] = useState([]);
+    const [isAllMuted, setAllMuted] = useState(true);
+    const [volume, setVolume] = useState(0);
+    const [preVolume, setPreVolume] = useState(50);
 
     useEffect(() => {
         const randomPerPage = Math.floor(Math.random() * 10) + 1;
@@ -21,11 +25,42 @@ function Home() {
                 console.log(err);
             });
     }, []);
+
+    const toggleAllVideosMute = () => {
+        if (isAllMuted) {
+            setAllMuted(false);
+            setVolume(preVolume);
+        } else {
+            setVolume(volume);
+            setAllMuted(true);
+            setVolume(0);
+        }
+        // setAllMuted((prevMuted) => !prevMuted);
+    };
+
+    const handleVolumeChange = (event) => {
+        const newVolume = parseFloat(event.target.value);
+        setVolume(newVolume);
+        if (newVolume === 0) {
+            setAllMuted(true);
+        } else {
+            setAllMuted(false);
+            setPreVolume(newVolume);
+        }
+    };
+
     return (
         <div>
             <div className={cx('wrapper')}>
                 {videoForYou.map((item, index) => (
-                    <Video data={item} key={item.id} />
+                    <Video
+                        key={item.id}
+                        data={item}
+                        isMuted={isAllMuted} // Truyền trạng thái âm lượng từ Home xuống Video
+                        toggleAllVideosMute={toggleAllVideosMute} // Truyền hàm để toggle âm lượng từ Home xuống Video
+                        volume={volume}
+                        onVolumeChange={handleVolumeChange}
+                    />
                 ))}
             </div>
         </div>
--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -20,12 +20,14 @@ import { useEffect, useState, useRef } from 'react';
 
 const cx = classNames.bind(styles);
 
-function Video({ data }) {
+function Video({ data, isMuted, toggleAllVideosMute, volume, onVolumeChange }) {
     const [playing, setPlaying] = useState(false);
-    const [muted, setMuted] = useState(false);
-    const [duration, setDuration] = useState(0);
-    const videoRef = useRef();
 
+    const [currentTime, setCurrentTime] = useState(0);
+    const [totalDuration, setTotalDuration] = useState(0);
+    const videoRef = useRef();
+    const [sliderValue, setSliderValue] = useState(0);
+    const [valueVolume, setValueVolume] = useState(0);
     // Add a state to track if the video is in the viewport
     const [inViewport, setInViewport] = useState(false);
 
@@ -57,10 +59,37 @@ function Video({ data }) {
         };
     }, []);
 
+    useEffect(() => {
+        const handleLoadedMetadata = () => {
+            if (videoRef.current) {
+                setTotalDuration(videoRef.current.duration);
+            }
+        };
+
+        const handleTimeUpdate = () => {
+            if (videoRef.current) {
+                setCurrentTime(Math.floor(videoRef.current.currentTime));
+                setSliderValue((videoRef.current.currentTime / totalDuration) * 100);
+            }
+        };
+
+        if (videoRef.current) {
+            videoRef.current.addEventListener('loadedmetadata', handleLoadedMetadata);
+            videoRef.current.addEventListener('timeupdate', handleTimeUpdate);
+        }
+
+        return () => {
+            if (videoRef.current) {
+                videoRef.current.removeEventListener('loadedmetadata', handleLoadedMetadata);
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+                videoRef.current.removeEventListener('timeupdate', handleTimeUpdate);
+            }
+        };
+    }, [totalDuration]);
+
     const handlePLayVideo = () => {
         videoRef.current.play();
         setPlaying(true);
-        console.log(playing);
     };
 
     const handlePauseVideo = () => {
@@ -75,21 +104,20 @@ function Video({ data }) {
             handlePLayVideo();
         }
     };
-
     const toggleVolumb = () => {
-        videoRef.current.muted = !videoRef.current.muted;
-        setMuted(videoRef.current.muted);
+        toggleAllVideosMute(); // Gọi hàm để toggle âm lượng cho tất cả video
+        setValueVolume(isMuted ? 0 : setValueVolume(volume));
     };
 
     useEffect(() => {
+        videoRef.current.muted = isMuted;
+    }, [isMuted]);
+
+    useEffect(() => {
         // Auto-play the first video when it is in the viewport
-        if (inViewport && !playing) {
-            setDuration(Math.round(videoRef.current.duration));
-
+        if (inViewport) {
             handlePLayVideo();
-        } else if (!inViewport && playing) {
-            setDuration(Math.round(videoRef.current.duration));
-
+        } else if (!inViewport) {
             handlePauseVideo();
         }
     }, [inViewport]);
@@ -98,11 +126,44 @@ function Video({ data }) {
         return <ShareVideo />;
     };
 
-    const [sliderValue, setSliderValue] = useState(50);
-
     const handleSliderChange = (event) => {
-        const value = event.target.value;
+        const value = parseFloat(event.target.value);
+        const time = (videoRef.current.duration / 100) * value;
+        videoRef.current.currentTime = time;
         setSliderValue(value);
+    };
+
+    const handleVolumeChange = (event) => {
+        onVolumeChange(event); // Gọi hàm để xử lý sự kiện thay đổi âm lượng từ Home
+        const newVolume = parseFloat(event.target.value) / 100;
+
+        videoRef.current.volume = newVolume; // Đặt âm lượng của video dựa trên giá trị từ input range
+        // setMuted(newVolume === 0); // Cập nhật trạng thái âm lượng (muted) dựa trên giá trị
+        setValueVolume(newVolume);
+    };
+
+    useEffect(() => {
+        videoRef.current.muted = isMuted;
+        videoRef.current.volume = volume / 100; // Đặt âm lượng của video dựa trên giá trị từ Home
+        setValueVolume(volume);
+    }, [isMuted, volume]);
+
+    const handleMouseUp = () => {
+        videoRef.current.currentTime = (sliderValue / 100) * totalDuration;
+    };
+
+    const formatTime = (currentTime) => {
+        const minutes = Math.floor(currentTime / 60);
+        const seconds = Math.floor(currentTime % 60);
+        return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+    };
+
+    // Thêm sự kiện ended cho video
+    const handleVideoEnd = () => {
+        // Đặt trạng thái playing thành false khi video kết thúc
+        setPlaying(true);
+        handlePLayVideo();
+        // Bạn có thể thực hiện các hành động khác khi video kết thúc ở đây
     };
 
     const valPercent = (sliderValue / 100) * 100;
@@ -150,8 +211,8 @@ function Video({ data }) {
                                     width="100%"
                                     height="100%"
                                     // controls
-                                    autoPlay={false}
                                     ref={videoRef}
+                                    onEnded={handleVideoEnd}
                                 >
                                     <source src={data.file_url} type="video/mp4" />
                                 </video>
@@ -160,8 +221,25 @@ function Video({ data }) {
                                     <div className={cx('play-icon')} onClick={toggleVideo}>
                                         {playing ? <PauseIcon /> : <PlayIcon />}
                                     </div>
-                                    <div className={cx('volume-icon')} onClick={toggleVolumb}>
-                                        {muted ? <MutedIcon /> : <VolumbIcon />}
+
+                                    <div className={cx('volume-icon')}>
+                                        <div className={cx('volume-icon-input')}>
+                                            <input
+                                                className={cx('volume-range')}
+                                                type="range"
+                                                min="0"
+                                                max="100"
+                                                step={0.1}
+                                                style={{
+                                                    background: `linear-gradient(to right, #fffe ${valueVolume}%, rgba(255, 255, 255, 0.34) ${valueVolume}%)`,
+                                                }}
+                                                value={volume} // Sử dụng giá trị âm lượng từ Home
+                                                onChange={handleVolumeChange} // Thêm sự kiện onChange để điều chỉnh âm lượng
+                                            />
+                                        </div>
+                                        <div className={cx('volumb-icon-btn')} onClick={toggleVolumb}>
+                                            {isMuted ? <MutedIcon /> : <VolumbIcon />}
+                                        </div>
                                     </div>
                                 </div>
                                 <div className={cx('time-input-range')}>
@@ -169,14 +247,18 @@ function Video({ data }) {
                                         type="range"
                                         min="0"
                                         max="100"
+                                        step={0.1}
                                         style={{
                                             background: `linear-gradient(to right, #fffe ${valPercent}%, rgba(255, 255, 255, 0.34) ${valPercent}%)`,
                                         }}
                                         value={sliderValue}
                                         className={cx('video-range')}
-                                        onChange={handleSliderChange}
+                                        onInput={handleSliderChange}
+                                        onMouseDown={handleMouseUp}
                                     />
-                                    <div className={cx('slider-value')}>{duration}</div>
+                                    <div className={cx('slider-value')}>
+                                        {formatTime(currentTime)}/{formatTime(totalDuration)}
+                                    </div>
                                 </div>
                             </div>
 
